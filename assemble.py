880b2dac924b56913fbcddef6c4df428db3617b9
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-07-15 23:08:26 -0500

    unflop images, unlink tmp files
    I ought to convert this script into a makefile

--- assemble.py
+++ assemble.py
@@ -1,6 +1,6 @@
-from sh import jpegtran, montage
+from sh import jpegtran, montage, convert
 from pprint import pprint
-from os import listdir
+from os import listdir, unlink
 
 
 
@@ -10,14 +10,36 @@ d=sorted(listdir('d'))
 
 assert len(i) == len(d)
 
+trash = []
+
 for n in range(len(i)):
-    I="i/trans_%s" % i[n]
-    D="d/trans_%s" % d[n]
+    I="tmp/i/trans_%s" % i[n]
+    D="tmp/d/trans_%s" % d[n]
+
+    # transverse op flops the photos
     jpegtran("-transverse", "i/%s" % i[n], _out=I)
     jpegtran("-transverse", "d/%s" % d[n], _out=D)
+    trash += [I, D]
 
+    montage_flopped = "stereo/flopped_%s_%s.jpg" \
+                      % (i[n].replace('.JPG',''),
+                         d[n].replace('.JPG',''))
     montage('-tile', '2x1',
             '-geometry','+0+0',
-            I, D, "stereo/%s_%s.jpg" % (i[n].replace('.JPG',''),d[n].replace('.JPG','')))
-            
+            I, D,
+            montage_flopped)
 
+    montage_unflopped = "stereo/%s_%s.jpg" \
+                        % (i[n].replace('.JPG',''),
+                           d[n].replace('.JPG',''))
+    convert(montage_flopped,
+            '-flop',
+            montage_unflopped)
+
+    trash.append(montage_flopped)
+    
+
+
+for f in trash:
+    unlink(f)
+    
